1c7a2661c62829c92124e1ffe022d5a992e4597c
marcopan <370510052@qq.com>
2014-05-11 17:51:34 +0800

    step1

---
+++ src/main/java/com/dinosaur/redistest/FirstApp.java
@@ -0,0 +1,12 @@
+package com.dinosaur.redistest;
+
+/**
+ * Created by pan on 2014/5/11.
+ */
+public class FirstApp {
+    public static void main(String args[]){
+        System.out.println(
+                "helle redis"
+        );
+    }
+}
